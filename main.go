b69f3317854dd49b5c3a174e1b66a2b667eab1af
Fahad Siddiqui <6371229+fahadsiddiqui@users.noreply.github.com>
2025-02-25 14:49:58 +0500

    New Schema Generation / Meta Data Fix

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq" // Import the PostgreSQL driver
 )
+
+func saveFile(filename string, data []byte) error {
+	return os.WriteFile(filename, data, 0644)
+}
+
+func saveMetadata(metadata SchemaDetails) {
+	b, err := json.Marshal(metadata)
+	if err != nil {
+		log.Fatalf("failed to marshal metadata: %v", err)
+	}
+
+	err = saveFile("metadata.json", b)
+	if err != nil {
+		log.Fatalf("failed to save metadata: %v", err)
+	}
+}
 
 func main() {
 	db, err := connectToDB()
@@ -13,12 +31,14 @@ func main() {
 	}
 	defer db.Close()
 
-	schemaDetails, err := fetchMetadata(db)
+	metadata, err := fetchMetadata(db)
 	if err != nil {
-		log.Fatalf("failed to fetch metadata: %v", err)
+		log.Fatalf("failed to build metadata: %v", err)
 	}
 
-	for _, table := range schemaDetails.Tables {
+	saveMetadata(metadata)
+
+	for _, table := range metadata.Tables {
 		tableData, err := FetchTableData(db, table)
 		if err != nil {
 			log.Fatalf("failed to fetch table data: %v", err)
